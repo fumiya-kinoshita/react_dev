af6c2f279a37ca78dea34ec579f398ca1d5d49c3
Fumiya Kinoshita <ir0213hi@ed.ritsumei.ac.jp>
2019-10-06 23:49:34 +0900

    Revert "Weekday.tsxにsetStateを追加"
    This reverts commit 339b925a0eea7ba0b80d8e394ac8c99c2d732f67.

--- src/components/Weekday.tsx
+++ src/components/Weekday.tsx
@@ -8,28 +8,21 @@ class Weekday extends React.Component<{}, WeekdayState> {
     constructor(props: {}) {
         super(props)
         this.state = {
-            weekday: "月曜日"
+            weekday: ""
         }
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e: React.FormEvent<HTMLSelectElement>) {
-        this.setState({ weekday: e.currentTarget.value });
     }
 
     render() {
         return(
             <div>
                 <form>
-                    <select value={this.state.weekday} onChange={this.handleChange} >
+                    <select>
                         <option value="月曜日">月曜日</option>
                         <option value="火曜日">火曜日</option>
                         <option value="水曜日">水曜日</option>
                         <option value="木曜日">木曜日</option>
                         <option value="金曜日">金曜日</option>
-                    </select><br />
-                    {this.state.weekday}
+                    </select>
                 </form>
             </div>
         )
